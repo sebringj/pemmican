49d135f7632330ba5669208863ae28b3fe5c2630
Jason Sebring <mail@jasonsebring.com>
2024-03-01 11:40:05 -0800

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,20 @@
+// crypto_utils_test.ts
+import { CryptoUtils } from './crypto_utils.ts';
+import {
+  assertEquals,
+  assertStringIncludes,
+} from 'https://deno.land/std/testing/asserts.ts';
+
+Deno.test('Generate Key Pair', async () => {
+  const { publicKeyPem, privateKeyPem } = await CryptoUtils.generateKeyPair();
+  assertStringIncludes(publicKeyPem, '-----BEGIN PUBLIC KEY-----');
+  assertStringIncludes(privateKeyPem, '-----BEGIN PRIVATE KEY-----');
+});
+
+Deno.test('Sign Data', async () => {
+  const { privateKeyPem } = await CryptoUtils.generateKeyPair();
+  const data = 'Hello, World!';
+  const { signatureBase64, timeStampISO } = await CryptoUtils.signData({ data, privateKeyPem });
+  assertEquals(typeof signatureBase64, 'string');
+  assertEquals(typeof timeStampISO, 'string');
+});
